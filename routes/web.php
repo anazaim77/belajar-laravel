d794b102c6daffad73fe2fd421759887f6821599
Fajar Pamungkas <fajar.pamungkas@Flip-MacBook-M1.local>
2023-06-22 23:17:17 +0700

    :construction: learn routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    return "About";
+});
+
+
+Route::view('/contact', 'contact', ['name' => 'fajar']);
+Route::redirect('/contactus', '/contact');
+
+Route::get('product/{id}', function ($id) {
+    return view('product.detail', ['id' => $id]);
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/profil', function () {
+        return 'profil admin';
+    });
+    Route::get('/about', function () {
+        return 'about admin';
+    });
+    Route::get('/contact', function () {
+        return 'contact admin';
+    });
+});
